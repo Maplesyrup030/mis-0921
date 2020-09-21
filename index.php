785c133748b01760a7839b03b8d987348c7c6b1d
Maple <gyps9607@gmail.com>
2020-09-21 16:28:27 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+    body {
+        font-family:微軟正黑體;
+    }
+</style>
+<title>莊絜涵0921</title>
+</head>
+<body>
+<h2>莊絜涵 git練習記錄</h2>
+<hr>
+<?php include "menu.php"; ?>
+<hr>
+<?php include "footer.php"; ?>
+</body>
+</html>
